608877e432e3dc529bf9bbd8cb1e336b79e8ec7b
Patrick Plattes <p.plattes@reply.de>
2015-05-24 18:31:53 +0200

    styles added to product listing sample data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create(title: 'Sommerkleid', description: 'Lorem ipsul', image_url: 'http://lorempixel.com/500/700/fashion/1/', price: '33.95')
+Product.create(title: 'Wintersocken', description: 'Lorem ipsul', image_url: 'http://lorempixel.com/500/700/fashion/2/', price: '49.95')
+Product.create(title: 'Jacke', description: 'Lorem ipsul', image_url: 'http://lorempixel.com/500/700/fashion/3/', price: '513.95')
+Product.create(title: 'Schuhe Damen', description: 'Lorem ipsul', image_url: 'http://lorempixel.com/500/700/fashion/4/', price: '111.95')
+Product.create(title: 'Shirt', description: 'Lorem ipsul', image_url: 'http://lorempixel.com/500/700/fashion/5/', price: '49.95')
+Product.create(title: 'Haare', description: 'Lorem ipsul', image_url: 'http://lorempixel.com/500/700/fashion/6/', price: '79.95')
+Product.create(title: 'Hoodie', description: 'Lorem ipsul', image_url: 'http://lorempixel.com/500/700/fashion/7/', price: '69.95')
+Product.create(title: 'Hose', description: 'Lorem ipsul', image_url: 'http://lorempixel.com/500/700/fashion/8/', price: '119.95')
+Product.create(title: 'Rock', description: 'Lorem ipsul', image_url: 'http://lorempixel.com/500/700/fashion/9/', price: '133.95')
+Product.create(title: 'Strumpfhose', description: 'Lorem ipsul', image_url: 'http://lorempixel.com/500/700/fashion/10/', price: '117.95')
+Product.create(title: 'Turnschuhe', description: 'Lorem ipsul', image_url: 'http://lorempixel.com/500/700/fashion/11/', price: '66.66')
+Product.create(title: 'Lederjacke', description: 'Lorem ipsul', image_url: 'http://lorempixel.com/500/700/fashion/12/', price: '33.95')
