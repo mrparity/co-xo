b8b1244961b4d9e3dbb480fb630a4af2e9064416
Patrick Plattes <p.plattes@reply.de>
2015-05-30 21:58:17 +0200

    added product id added validation for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+  validates :product_id, uniqueness: true
+  validates :product_id, :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_tan_or_equel_to: 0.01}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150524164334) do
+ActiveRecord::Schema.define(version: 20150525094158) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150524164334) do
     t.decimal  "price",       precision: 12, scale: 4
     t.datetime "created_at",                           null: false
     t.datetime "updated_at",                           null: false
+    t.string   "product_id"
   end
 
 end
