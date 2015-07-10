6ceb3e543313bc465cb96d66357a9ed702a8799d
Patrick Plattes <p.plattes@reply.de>
2015-07-10 18:41:01 +0200

    fix validation typo

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
   validates :product_uid, uniqueness: true
   validates :product_uid, :title, :description, :image_url, presence: true
-  validates :price, numericality: {greater_tan_or_equel_to: 0.01}
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
 end
